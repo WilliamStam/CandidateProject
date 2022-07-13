cede40ba95e05863139269eb54428c3eb96defd2
William Stam <william@munsoft.co.za>
2022-07-13 14:37:28 +0200

    mysql setup sql fixes

--- docker/mysql/setup.sql
+++ docker/mysql/setup.sql
@@ -1,42 +1,31 @@
 CREATE TABLE `logs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT primary key,
-  `level` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `log` mediumtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `context` mediumtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` datetime DEFAULT current_timestamp()
+  `id` INT NOT NULL AUTO_INCREMENT primary key,
+  `level` VARCHAR(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `log` MEDIUMTEXT COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `context` MEDIUMTEXT COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` DATETIME DEFAULT current_timestamp()
 );
 
 CREATE TABLE `services` (
-  `id` int(11) NOT NULL AUTO_INCREMENT primary key,
-  `service` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `id` INT NOT NULL AUTO_INCREMENT primary key,
+  `service` VARCHAR(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 );
 
-
 CREATE TABLE `subscriptions` (
-    `uuid` varchar(36) DEFAULT (uuid_to_bin(uuid())) not null primary key,
-    `msisdn` varchar(11) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `service_id` int(11) DEFAULT NULL,
-    `charged_at` datetime DEFAULT NULL,
-    `canceled_at` datetime DEFAULT NULL,
-    `is_active` tinyint(1) DEFAULT 0,
-    `created_at` datetime DEFAULT current_timestamp(),
-    `updated_at` datetime DEFAULT current_timestamp(),
+    `uuid` BINARY(16) DEFAULT (uuid_to_bin(uuid())) not null primary key,
+    `msisdn` VARCHAR(11) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `service_id` INT DEFAULT NULL,
+    `charged_at` DATETIME DEFAULT NULL,
+    `canceled_at` DATETIME DEFAULT NULL,
+    `is_active` TINYINT(1) DEFAULT 0,
+    `created_at` DATETIME DEFAULT current_timestamp(),
+    `updated_at` DATETIME DEFAULT current_timestamp(),
     UNIQUE KEY `unique` (`msisdn`,`service_id`),
     KEY `msisdn_id` (`msisdn`),
     KEY `service_id` (`service_id`)
 );
 
-
-
-
-INSERT INTO `services` (`id`, `service`) VALUES
-(1, 'netflix'),
-(2, 'youtube')
-;
-
-
-
-CREATE OR REPLACE VIEW VIEW_subscriptions AS
+CREATE VIEW view_subscriptions AS
 SELECT
     subscriptions.`uuid`,
     subscriptions.`msisdn`,
@@ -51,4 +40,10 @@ FROM
     subscriptions
         JOIN services
             ON services.id = subscriptions.service_id
+;
+
+
+INSERT INTO `services` (`id`, `service`) VALUES
+(1, 'netflix'),
+(2, 'youtube')
 ;
